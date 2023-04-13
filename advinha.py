a11323c1f2aabd5628d37ef71d025a67d29a83ca
joaosilvamartins <102842104+joaosilvamartins@users.noreply.github.com>
2023-04-13 19:39:01 -0300

    Add files via upload

---
+++ advinha.py
@@ -0,0 +1,16 @@
+import random
+
+print('Você tem 3 chances para acertar o número que o computador escolheu!')
+print('Vamos lá?')
+prim = print('Primeira chance!')
+num = int(input('Digite seu número: '))
+x = random.randint(1, 10)
+
+for i in range(2):
+    if num == x:
+        print('Você acertou!')
+        print('Computador também escolheu {}'.format(x))
+        exit()
+    else:
+        print("Você errou, tente novamente!")
+        num = int(input("Digite seu número: "))
